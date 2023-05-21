47ab001d65445d12f12e3b90655cf88415057ca7
Sidharth Sharma <sharma.drmadhu@gmail.com>
2023-05-21 20:02:19 +0530

    Update BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,9 +81,6 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
 BOARD_SUPER_PARTITION_GROUPS := oplus_dynamic_partitions
-BOARD_OPLUS_DYNAMIC_PARTITIONS_PARTITION_LIST := system system system_ext vendor vendor product product odm odm my_product my_product my_engineering my_engineering my_company my_company my_carrier my_carrier my_region my_region my_heytap my_heytap my_stock my_stock my_preload my_preload my_manifest my_manifest my_bigball my_bigball
-BOARD_OPLUS_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
-
 # Platform
 TARGET_BOARD_PLATFORM := mt6877
 
